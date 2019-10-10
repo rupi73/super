d7eb0b71fb1a1a13589b9f2c942b3410e03f0157
mohinder909 <singhmonu9815@gmail.com>
2019-10-10 18:21:45 +0530

    makes form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header  bg-light text-center"><img src="{{asset('assets/images/chhapai.png')}}" height="40px" width="100px"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
