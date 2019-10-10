51b00bd1f9ed6cc27be2c9d76c350f3db4bd913c
mohinder909 <singhmonu9815@gmail.com>
2019-10-10 12:59:47 +0530

    papers index

--- resources/views/papers/create.blade.php
+++ resources/views/papers/create.blade.php
@@ -14,82 +14,133 @@
 
     
     <fieldset>
-    <div class="panel panel-transparent">
-                                            <legend>Your Account</legend>
-                                            <div class="panel-body"><br>
-                                               
-                                                <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox4" value="1" checked="">
-                                                    <label for="checkbox4">Prints</label>
+        
+                                                    <div class="form-group">
+                                                        <label class="block">Print</label>
+                                                        <div class="clip-radio radio-primary">
+                                                        <input type="radio" id="male" name="gender" value="male" checked="checked">
+                                                            <label for="male">None</label>
+                                                            <input type="radio" id="female" name="gender" value="female">
+                                                            <label for="female">Single side</label>
+                                                            <input type="radio" id="male" name="gender" value="male" checked="">
+                                                            <label for="male">Both side</label>
+                                                           
+                                                        </div>
+                                                    </div>
+</fieldset>
+                                    <div class="col-md-6">
+                                        
+                                            <legend>Quantity</legend>
+                                            <div class="row">
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label>100</label>
+                                                        <input type="text" name="firstName" class="form-control" placeholder="price" >
+                                                    </div>
                                                 </div>
-                                                <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox5" value="1">
-                                                    <label for="checkbox5">Both sides</label>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label class="control-label">250</label>
+                                                        <input type="text" name="lastName" class="form-control" placeholder="price" >
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="form-group">
+                                                        <label class="control-label">500</label>
+                                                        <input type="text" name="lastName" class="form-control" placeholder="price" >
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-</fieldset>
-
-
-<div class="panel-heading">
-<h5 class="panel-title">Quantity</h5></div>
-<div class="form-group">
-                                                        <div class="row">
-                                                            <label class="col-sm-2 col-form-label text-right" for="inputEmail3">100</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" placeholder="price" id="inputEmail3" class="form-control">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <div class="row">
-                                                            <label class="col-sm-2 col-form-label text-right" for="inputPassword3">250</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" placeholder="price" id="inputPassword32" class="form-control">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group">
-                                                        <div class="row">
-                                                            <label class="col-sm-2 col-form-label text-right" for="inputPassword3">500</label>
-                                                            <div class="col-sm-10">
-                                                                <input type="text" placeholder="price" id="inputPassword32" class="form-control">
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                    <div class="col-sm-6">
+</div>
+                            
+                                     <div class="row">
+                                    <div class="col-sm-3">
                                         <div class="panel panel-transparent">
                                             <div class="panel-heading">
                                                 <h5 class="panel-title">Treatments</h5></div>
                                             <div class="panel-body">
                                                 
-                                                <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox4" value="1" checked="">
-                                                    <label for="checkbox4">Checkbox 1</label>
+                                            <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox6" value="1">
+                                                    <label for="checkbox6">Edgepaint</label>
                                                 </div>
-                                                <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox5" value="1">
-                                                    <label for="checkbox5">Checkbox 2</label>
+                                                <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox7" value="1">
+                                                    <label for="checkbox7">Electroplating</label>
+                                                </div>
+                                                <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox7" value="1">
+                                                    <label for="checkbox7">Raised Spot Gloss</label>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+
+                                    <div class="col-sm-3">
+                                        <div class="panel panel-transparent">
+                                            <div class="panel-heading">
+                                               
+                                            <div class="panel-body">
+                                           
+                                                <div class="checkbox clip-check check-primary mt-5">
+                                                    <input type="checkbox" id="checkbox8" value="1">
+                                                    <label for="checkbox8">Laser Cutting</label>
+                                                </div>
+                                                <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox9" value="1" >
+                                                    <label for="checkbox9">Laser Engrave</label>
+                                                </div>
+                                                <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox7" value="1">
+                                                    <label for="checkbox7">Roundcorner</label>
                                                 </div>
                                             </div>
                                         </div>
+                                    </div>
 
-                                        <div class="col-sm-6">
-                                        <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox4" value="1" checked="">
-                                                    <label for="checkbox6">Checkbox 1</label>
-                                                </div>
-                                                <div class="checkbox clip-check check-primary checkbox-inline">
-                                                    <input type="checkbox" id="checkbox5" value="1">
-                                                    <label for="checkbox7">Checkbox 2</label>
-                                                </div>
-</div>
+
+
+
+
+
+
+
 
 </div>
-                                        
+
+                                <div class="col-sm-3 ml-2">
+                                        <div class="panel panel-transparent">
+                                            <div class="panel-heading">
+                                               
+                                            <div class="panel-body">
+                                                
+                                            <div class="checkbox clip-check check-primary mt-5">
+                                                    <input type="checkbox" id="checkbox8" value="1">
+                                                    <label for="checkbox8">Foiling</label>
+                                                </div>
+                                                <div class="checkbox clip-check check-primary">
+                                                    <input type="checkbox" id="checkbox9" value="1" >
+                                                    <label for="checkbox9">Letterpress</label>
+                                                </div>
+                                             
+                                            </div>
+                                        </div>
+                                    </div>
+                    </div>
+
+    
+                                 
+
+                                   
                                        
 </div>
+<button type="button" class="btn btn-outline-primary btn-lg">Save</button> 
+</div>
+
 </div>
 
     
---
+++ resources/views/papers/index.blade.php
@@ -0,0 +1,100 @@
+@extends('template')
+@section('content')
+<div class="container">
+    
+    <div class="row">
+        <h2>Paper Index</h2>
+        <!--matt finish-->
+        <div class="col-md-12 mt-5">
+        <div class="card" >
+  <div class="card-body">
+    <h4 class="card-title">MATT FINISH</h4>
+    <h6 class="card-text">
+        DESCRIPTION
+</h6>
+<p><b>Treatment :</b>	
+Foiling, Raised Spot Gloss
+
+</p>
+<p><b>Quantity :</b><b>100 :</b> ₹2.75 per card ,<b>250 : </b>₹2.60 per card ,<b>500 :</b>₹2.40 per card ,<b>750 : </b>₹2.20 per card ,<b>1000 : </b> ₹2.00 per card ,<b>2000 : </b> ₹1.90 per card ,<b>4000 : </b>₹1.80 per card
+</p>
+<p>Give a personal touch and shine free finish with easy readability. Experience the beautiful smooth feel on your card with a matte surface with rich look and feel.
+
+</p>
+    <a href="#" class="card-link">Edit</a>
+    
+  </div>
+</div>
+
+        </div>
+  
+
+
+
+
+
+ 
+        <!--soft suede-->
+        <div class="col-md-12 mt-5">
+        <div class="card" >
+  <div class="card-body">
+    <h4 class="card-title">SOFT SUEDE</h4>
+    <h6 class="card-text">
+        DESCRIPTION
+</h6>
+<p><b>Treatment :</b>	
+Edgepaint, Electroplating, Foiling, Letterpress, Raised Spot Gloss
+
+</p>
+<p><b>Quantity - 100 : ₹400 , 250 : ₹350 , 500: ₹300 , 750 : ₹275 , 1000 : ₹260  , 2000 : ₹2.60 , 400 : ₹220 </b></p>
+
+<p>Redefine your cardstock with fine leather like feel with combinations of colors Red, Black, White or Custom.
+
+</p>
+    <a href="#" class="card-link">Edit</a>
+    
+  </div>
+</div>
+
+        </div>
+  
+    </div>
+
+
+
+
+     <!--TEXTURE-->
+     <div class="col-md-12 mt-5">
+        <div class="card" >
+  <div class="card-body">
+    <h4 class="card-title">TEXTURE</h4>
+    <h6 class="card-text">
+        DESCRIPTION
+</h6>
+<p><b>Treatment :</b>	
+Edgepaint, Electroplating, Foiling, Letterpress, Raised Spot Gloss
+
+</p>
+<p><b>Quantity - 100:₹4.00 per card , 250:₹3.50 per card , 500:₹3.00 per card , 750:₹2.75 per card , 1000:₹2.50 per card , 2000:₹2.00 per card , 4000:₹1.75per card</b></p>
+
+<p>Have an outstanding textured business cards with the most elegant and classy designs.</p>
+    <a href="#" class="card-link">Edit</a>
+    
+  </div>
+</div>
+
+        </div>
+  
+    </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+@endsection
