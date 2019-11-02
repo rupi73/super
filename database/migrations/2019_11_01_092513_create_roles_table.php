426e7e83336abd33de682d3fa1282025c21162bd
Rupinder.Singh <rupinder.singh@terasofts.org>
2019-11-02 18:41:04 +0530

    migration bug rectified

--- database/migrations/2019_11_01_092513_create_roles_table.php
+++ database/migrations/2019_11_01_092513_create_roles_table.php
@@ -25,7 +25,7 @@ class CreateRolesTable extends Migration
             $table->timestamps();
             $table->integer('user_id')->unsigned();
             $table->tinyInteger('role_id')->unsigned();
-            $table->primary(['user_id','role_id']);
+            //$table->primary(['user_id','role_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade')->onUpdate('cascade');
         });
