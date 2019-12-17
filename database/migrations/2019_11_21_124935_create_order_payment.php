93b0b3d783d3653bb0f61825b93c6c1f3420e377
Rupinder.Singh <rupinder.singh@terasofts.org>
2019-12-17 17:38:57 +0530

    migration name changed

--- database/migrations/2019_11_21_124935_create_order_payment.php
+++ database/migrations/2019_11_21_124935_create_order_payment.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBillingOrder extends Migration
+class CreateOrderPayment extends Migration
 {
     /**
      * Run the migrations.
